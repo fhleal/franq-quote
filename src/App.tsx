0f6e579a18efcc439bc9bf51f55c6d029c201a7d
Felipe Leal <felipehenriqueleal@gmail.com>
2025-04-08 15:03:24 -0300

    chore: testing automated deploy

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './index.css';
 
 function App() {
@@ -8,14 +6,6 @@ function App() {
 
   return (
     <div className="flex flex-col items-center text-center p-9">
-      <div>
-        <a href="https://vite.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1 className="text-4xl text-blue-500">Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
